093be8be0572c51c1852df6716f9c9b3543f1ff2
Malte Bjoerklund <maltebjoerklund@gmail.com>
2024-10-29 19:15:28 +0100

    clickable towers for movement of disks

--- controller.js
+++ controller.js
@@ -12,10 +12,10 @@ class Controller {
     this.towerHeight = towerHeight;
     this.towers = [];
     this.moves = 0;
+    this.clickedTower = 0;
   }
 
   init() {
-    //Add event listeners
     this.initTowers();
     this.displayBoard();
   }
@@ -24,9 +24,35 @@ class Controller {
     for (let i = 0; i < this.towerAmount; i++) {
       this.towers[i] = new Stack();
     }
-    //Pushes the disks to the first tower
+    //Initializes the disks on the first tower
     for (let i = this.towerHeight; i > 0; i--) {
       this.towers[0].push(i);
+    }
+  }
+
+  eventListeners() {
+    for (let i = 1; i <= this.towerAmount; i++) {
+      document.querySelector(`#tower${i}`).addEventListener("click", (e) => {
+        e.preventDefault();
+        this.towerClicked(i);
+      });
+    }
+  }
+
+  towerClicked(tower) {
+    console.log(`Tower ${tower} clicked!`);
+
+    if (this.clickedTower === 0) {
+      document.querySelector("#towerFrom").textContent = "(click tower)";
+      document.querySelector("#towerTo").textContent = "(click tower)";
+      this.clickedTower = tower;
+      document.querySelector("#towerFrom").textContent = tower;
+    } else {
+      const prevClickedTower = this.clickedTower;
+      this.clickedTower = tower;
+      document.querySelector("#towerTo").textContent = tower;
+      this.moveTopDisk(prevClickedTower, this.clickedTower);
+      this.clickedTower = 0;
     }
   }
 
@@ -40,18 +66,17 @@ class Controller {
     ) {
       towerTo.push(towerFrom.pop());
       this.moves++;
+      this.displayBoard();
+      return 1;
     } else {
       console.log("Invalid move");
+      this.displayBoard();
+      return -1;
     }
-    this.displayBoard();
   }
 
   displayBoard() {
     document.querySelector("#game").innerHTML = `
-    <h3>
-      Moving disk from tower: <span id="towerFrom">(click tower)</span> to tower:
-      <span id="towerTo">(click tower)</span>
-    </h3>
     <div id="towers" style="grid-template-columns: repeat(${this.towerAmount}, 1fr)"></div>
     `;
     for (let i = 0; i < this.towerAmount; i++) {
@@ -72,5 +97,6 @@ class Controller {
           );
       }
     }
+    this.eventListeners();
   }
 }
