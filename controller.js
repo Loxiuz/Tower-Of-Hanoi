e5dc7a98ede6090b601b27dcb1fe393406fcbe04
Malte Bjoerklund <maltebjoerklund@gmail.com>
2024-11-06 20:45:41 +0100

    Implementation of self-solving

--- controller.js
+++ controller.js
@@ -1,20 +1,23 @@
 import Stack from "./stack.js";
 
 window.addEventListener("load", () => {
-  const controller = new Controller(8);
+  const controller = new Controller(8, 200);
   controller.init();
   window.controller = controller;
 });
 
 class Controller {
-  constructor(towerHeight = 4, towerAmount = 3) {
+  //The solver only works with three towers
+  constructor(towerHeight = 4, moveDelay = 300, towerAmount = 3) {
     this.towerAmount = towerAmount;
     this.towerHeight = towerHeight;
+    this.moveDelay = moveDelay;
     this.towers = [];
     this.moves = 0;
     this.clickedTower = 0; //latest tower clicked
     //Bind class to relevant functions
     this.boundResetBtnClicked = this.resetBtnClicked.bind(this);
+    this.handleSolveClicked = this.handleSolveClicked.bind(this);
     this.boundTowerClicked = this.towerClicked.bind(this);
   }
 
@@ -40,8 +43,11 @@ class Controller {
       tower.addEventListener("click", this.boundTowerClicked);
     }
     const resetBtn = document.querySelector("#resetBtn");
+    const solveBtn = document.querySelector("#solveBtn");
     resetBtn.removeEventListener("click", this.boundResetBtnClicked);
+    solveBtn.removeEventListener("click", this.handleSolveClicked);
     resetBtn.addEventListener("click", this.boundResetBtnClicked);
+    solveBtn.addEventListener("click", this.handleSolveClicked);
   }
 
   resetBtnClicked(e) {
@@ -52,6 +58,12 @@ class Controller {
     this.clickedTower = 0;
     document.querySelector("#movingFeedback").innerHTML = "";
     this.display();
+  }
+
+  handleSolveClicked(e) {
+    console.log("Solve button clicked");
+    e.preventDefault();
+    this.solver();
   }
 
   //When you click a tower
@@ -86,14 +98,46 @@ class Controller {
     document.querySelector("#movingFeedback").textContent = "Game Won!";
   }
 
+  //Only works correctly with three towers
   isGameOver() {
-    return this.towers[this.towerAmount - 1].size() === this.towerHeight;
+    return (
+      this.towers[1].size() === this.towerHeight ||
+      this.towers[2].size() === this.towerHeight
+    );
+  }
+
+  //Solves the game from any position.
+  //Only works with three towers and for the moment only even amount of disks.
+
+  async solver() {
+    if (this.isGameOver()) {
+      this.handleGameOver();
+    } else {
+      if (!this.moveTopDisk(1, 2)) {
+        this.moveTopDisk(2, 1);
+      }
+      await this.delay(this.moveDelay);
+      if (!this.moveTopDisk(1, 3)) {
+        this.moveTopDisk(3, 1);
+      }
+      await this.delay(this.moveDelay);
+      if (!this.moveTopDisk(2, 3)) {
+        this.moveTopDisk(3, 2);
+      }
+      await this.delay(this.moveDelay);
+      await this.solver();
+    }
+  }
+
+  delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   //"From" and "to" is the tower id
   moveTopDisk(from, to) {
     const towerFrom = this.towers[from - 1];
     const towerTo = this.towers[[to - 1]];
+    // console.log(`From: ${from}, To: ${to}`);
     if (this.isValidMove(towerFrom, towerTo)) {
       towerTo.push(towerFrom.pop());
       this.moves++;
@@ -101,10 +145,12 @@ class Controller {
       if (this.isGameOver()) {
         this.handleGameOver();
       }
+      return true;
     } else {
       console.log("Invalid move");
       document.querySelector("#movingFeedback").textContent = "Invalid Move";
       this.display();
+      return false;
     }
   }
 
